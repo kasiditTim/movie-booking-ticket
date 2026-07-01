2f0227a77fc9b069fa6a815537256cc1eb05b492
kasidit_tim <joker642544@gmail.com>
2026-07-02 00:35:17 +0700

    feat: add divider color to AppColors

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -11,6 +11,8 @@ class AppColors {
   static const card = Color(0xFF1C1C1C);
   static const cardSelected = Color(0xFF261D08);
 
+  static const divider = Color(0xFF4A4A4A);
+
   static const white = Color(0xFFF2F2F2);
   static const grey = Color(0xFFBFBFBF);
   static const darkGrey = Color(0xFF262626);
