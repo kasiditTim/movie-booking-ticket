3f6054418dc7cd629febff57a61c4a187d76fe81
kasidit_tim <joker642544@gmail.com>
2026-07-02 00:34:48 +0700

    feat: add gap constants

--- lib/core/theme/app_spacing.dart
+++ lib/core/theme/app_spacing.dart
@@ -7,8 +7,10 @@ class Gap {
   static const h16 = SizedBox(height: 16);
   static const h24 = SizedBox(height: 24);
   static const h32 = SizedBox(height: 32);
+  static const h64 = SizedBox(height: 64);
 
   static const w4 = SizedBox(width: 4);
   static const w8 = SizedBox(width: 8);
   static const w16 = SizedBox(width: 16);
+  static const w24 = SizedBox(width: 24);
 }
